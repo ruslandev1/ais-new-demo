b6ed8eaa32f04bb636e9cdc2df00953efb3d427b
ruslandev1 <ruslanqurbanovdev@gmail.com>
2023-09-01 09:55:02 +0400

    fixing alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,10 +1,10 @@
 import { Alert, Text } from '@mantine/core';
 import { AlertCircle } from 'tabler-icons-react';
 
-export default function AlertForError({ errorType}) {
+export default function AlertForError({ error}) {
     return (
         <Text truncate title="Xəta" color="red" radius="md" sx={{ marginTop: 25 }}>
-            {errorType &&  errorType}
+            {error.summary}
         </Text>
     );
 }
